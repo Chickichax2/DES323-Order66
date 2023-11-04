d2638fb5277edfe16eff3d92a30f15a3f0d7411a
Nina_4009 <6322774009@g.siit.tu.ac.th>
2023-11-04 18:35:19 +0700

    Unfinished
    In-class exercises are used as a reference

--- DES323/my_app/models.py
+++ DES323/my_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class dairy_dataset(models.Model):
+    location = models.CharField(max_length=255)
+    tot_land_area = models.FloatField()
+    num_cows = models.IntegerField()
+    price = models.FloatField()
+    recording_date = models.DateField()
+    farm_size = models.CharField(max_length=255)
+    product_type = models.CharField(max_length=255)
+    quantity = models.IntegerField()
--- DES323/my_app/views.py
+++ DES323/my_app/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+import pandas as pd
+from my_app.models import dairy_dataset
 # Create your views here.
 
 def index(request):
     return render(request, "index.html")
 
-
 def about(request):
     return render(request, "about.html")
-
 
 def user(request):
     return render(request, "user.html")
@@ -28,3 +29,26 @@ def contact(request):
 def result(request):
     return render(request, "result.html")
 
+def import_dairy_data(request):
+    csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRsQsF25947ZGBAMUS_ljKLeV4bFzPSOf_wShl8_mqXOwvohpmxXq0bu-zFfGn2ppf4TmS1lNIl7aLU/pub?output=csv"
+    df = pd.read_csv(csv_url)
+    dairy_dataset = df[["Location", "Total Land Area (acres)", "Number of Cows", "Price per Unit", "Date", "Farm Size", "Product Name", "Quantity (liters/kg)"]]
+    success = []
+    errors = []
+    for index, row in dairy_dataset.iterrows():
+        instance = dairy_dataset(
+            location = row['Location'],
+            tot_land_area = row['Total Land Area (acres)'],
+            num_cows = int(row['Number of Cows']),
+            price = row['Price per Unit'],
+            recording_date = row['Date'],
+            farm_size = row['Farm Size'],
+            product_type = row['Product Name'],
+            quantity = int(row['Quantity (liters/kg)'])
+        )
+        try:
+            instance.save()
+            success.append(index)
+        except:
+            errors.append(index)
+    return JsonResponse({"success_indexes":success, "error_indexes":errors})
