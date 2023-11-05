ddd6e2cc6858a6ff0c01cb4f5f74c2b1ddd58bde
Nina_4009 <97821967+melonbread404@users.noreply.github.com>
2023-11-05 09:57:01 +0000

    a

--- DES323/my_app/models.py
+++ DES323/my_app/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 class dairy_dataset(models.Model):
     location = models.CharField(max_length=255)
-    tot_land_area = models.FloatField(default=0)
-    num_cows = models.IntegerField(default=0)
-    price = models.FloatField(default=0)
+    tot_land_area = models.FloatField()
+    num_cows = models.IntegerField()
+    price = models.FloatField()
     recording_date = models.DateField()
     farm_size = models.CharField(max_length=255)
     product_type = models.CharField(max_length=255)
-    quantity = models.IntegerField(default=0)
+    quantity = models.IntegerField()
     added_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
     added_date = models.DateTimeField(default=timezone.now)
